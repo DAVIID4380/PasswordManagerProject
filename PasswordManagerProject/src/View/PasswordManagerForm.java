3688cad088f12bfc014c5aa9054a40a353991a65
David Botina <davidbotina4380@gmail.com>
2024-12-07 14:41:40 -0500

    Update PasswordManagerForm

--- PasswordManagerProject/src/View/PasswordManagerForm.java
+++ PasswordManagerProject/src/View/PasswordManagerForm.java
@@ -32,6 +32,7 @@ import java.util.List;
  *
  * @author David Botina
  */
+
 public class PasswordManagerForm extends JFrame {
 
     private JTable passwordTable;
@@ -77,3 +78,193 @@ public class PasswordManagerForm extends JFrame {
                 new AddPasswordForm(PasswordManagerForm.this, userId, tableModel).setVisible(true);
             }
         });
+
+        deleteButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                deleteAllPasswords();
+            }
+        });
+
+        showPasswordButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                showSelectedPassword();
+            }
+        });
+
+        searchField.getDocument().addDocumentListener(new DocumentListener() {
+            public void insertUpdate(DocumentEvent e) {
+                searchPasswords(searchField.getText());
+            }
+
+            public void removeUpdate(DocumentEvent e) {
+                searchPasswords(searchField.getText());
+            }
+
+            public void changedUpdate(DocumentEvent e) {
+                searchPasswords(searchField.getText());
+            }
+        });
+
+        passwordTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent event) {
+                if (!event.getValueIsAdjusting() && passwordTable.getSelectedRow() != -1) {
+                    showPasswordButton.setEnabled(true);
+                } else {
+                    showPasswordButton.setEnabled(false);
+                }
+            }
+        });
+
+        loadPasswords();
+        createPopupMenu();
+    }
+
+    private void loadPasswords() {
+        try (Connection connection = ConnectionDB.getConnection()) {
+            String query = "SELECT * FROM passwordentries WHERE userId = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, userId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String title = resultSet.getString("title");
+                String username = resultSet.getString("username");
+                String encryptedPassword = resultSet.getString("encryptedPassword");
+                String url = resultSet.getString("url");
+                String notes = resultSet.getString("notes");
+                passwordEntries.add(new PasswordEntry(id, userId, title, username, encryptedPassword, url, notes));
+            }
+
+            tableModel.fireTableDataChanged();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void deleteAllPasswords() {
+        try (Connection connection = ConnectionDB.getConnection()) {
+            String query = "DELETE FROM passwordentries WHERE userId = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, userId);
+            preparedStatement.executeUpdate();
+
+            passwordEntries.clear();
+            tableModel.fireTableDataChanged();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void searchPasswords(String keyword) {
+        passwordEntries.clear();
+        try (Connection connection = ConnectionDB.getConnection()) {
+            String query = "SELECT * FROM passwordentries WHERE userId = ? AND (title LIKE ? OR username LIKE ? OR url LIKE ? OR notes LIKE ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, userId);
+            String searchPattern = "%" + keyword + "%";
+            preparedStatement.setString(2, searchPattern);
+            preparedStatement.setString(3, searchPattern);
+            preparedStatement.setString(4, searchPattern);
+            preparedStatement.setString(5, searchPattern);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String title = resultSet.getString("title");
+                String username = resultSet.getString("username");
+                String encryptedPassword = resultSet.getString("encryptedPassword");
+                String url = resultSet.getString("url");
+                String notes = resultSet.getString("notes");
+                passwordEntries.add(new PasswordEntry(id, userId, title, username, encryptedPassword, url, notes));
+            }
+
+            tableModel.fireTableDataChanged();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showSelectedPassword() {
+        int selectedRow = passwordTable.getSelectedRow();
+        if (selectedRow >= 0) {
+            PasswordEntry entry = passwordEntries.get(selectedRow);
+            try {
+                // Instanciar el servicio de encriptación con la clave fija de 16 bytes
+                EncryptionService encryptionService = new EncryptionService();
+                String decryptedPassword = encryptionService.decrypt(entry.getEncryptedPassword());
+
+                JOptionPane.showMessageDialog(this, "Password: " + decryptedPassword, "Decrypted Password", JOptionPane.INFORMATION_MESSAGE);
+            } catch (Exception e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(this, "Error decrypting password.", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    private void deletePassword(int id) {
+        try (Connection connection = ConnectionDB.getConnection()) {
+            String query = "DELETE FROM passwordentries WHERE id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createPopupMenu() {
+        popupMenu = new JPopupMenu();
+
+        JMenuItem editItem = new JMenuItem("EDITAR");
+        editItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int selectedRow = passwordTable.getSelectedRow();
+                if (selectedRow >= 0) {
+                    PasswordEntry entry = passwordEntries.get(selectedRow);
+                    new EditPasswordForm(PasswordManagerForm.this, entry, tableModel).setVisible(true);
+                }
+            }
+        });
+        popupMenu.add(editItem);
+
+        JMenuItem deleteItem = new JMenuItem("ELIMINAR");
+        deleteItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int selectedRow = passwordTable.getSelectedRow();
+                if (selectedRow >= 0) {
+                    int id = passwordEntries.get(selectedRow).getId();
+                    deletePassword(id);
+                    tableModel.removeRow(selectedRow);
+                }
+            }
+        });
+        popupMenu.add(deleteItem);
+
+        passwordTable.setComponentPopupMenu(popupMenu);
+
+        passwordTable.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                int row = passwordTable.rowAtPoint(e.getPoint());
+                passwordTable.getSelectionModel().setSelectionInterval(row, row);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (e.isPopupTrigger()) {
+                    int row = passwordTable.rowAtPoint(e.getPoint());
+                    passwordTable.getSelectionModel().setSelectionInterval(row, row);
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new UserForm().setVisible(true);
+            }
+        });
+    }
+}
